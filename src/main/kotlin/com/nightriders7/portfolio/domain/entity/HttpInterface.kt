9c6e597656a343f5dbf97a00bbbf0a29344ca9ae
student1 <makesense7899@gmail.com>
2024-10-08 20:56:28 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/nightriders7/portfolio/domain/entity/HttpInterface.kt
+++ src/main/kotlin/com/nightriders7/portfolio/domain/entity/HttpInterface.kt
@@ -1,14 +1,30 @@
 package com.nightriders7.portfolio.domain.entity
 
 import jakarta.persistence.*
+import jakarta.servlet.http.HttpServletRequest
 
 @Entity
-class HttpInterface : BaseEntity() {
+class HttpInterface(httpServletRequest: HttpServletRequest) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "http_interface_id")
     var id: Long? = null
 
+    var cookies: String? = httpServletRequest.cookies
+        ?.map { "${it.name}:${it.value}" }
+        ?.toString() // 쿠키를 문자열로 반환
+
+    var referer: String? = httpServletRequest.getHeader("referer")
+
+    var localAddress: String? =  httpServletRequest.localAddr
+
+    var remoteAddr: String? = httpServletRequest.remoteAddr
+
+    var remoteHost: String? = httpServletRequest.remoteHost
+
+    var requestUri: String? = httpServletRequest.requestURI
+
+    var userAgent: String? = httpServletRequest.getHeader("user-agent")
 
 }
